e1ffa30f8120518f3a4d7500c251da9a6ed37ba4
shimmieblum <shimmieblum@gmail.com>
2021-04-22 15:14:41 +0100

    updated with correct secret key details

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,17 +11,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import secrets
+import my_secrets
+import pytz
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets.DJANGO_SECRET_KEY
+SECRET_KEY = my_secrets.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'BST'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
